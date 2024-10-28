0b9b69f55d3fcc4cb3cf6ac596d43a16a4868bed
stanley <greenstan@naver.com>
2024-10-28 16:49:15 +0900

    Stanley Ajax module 외 7개 파일

--- src/main/java/com/team3/controller/member/UserAuthStartController.java
+++ src/main/java/com/team3/controller/member/UserAuthStartController.java
@@ -1,6 +1,7 @@
 package com.team3.controller.member;
 
 import com.team3.controller.SuperClass;
+import com.team3.utility.SendMailUtil;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +12,14 @@ public class UserAuthStartController extends SuperClass {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		super.doGet(request, response);
+		
+	    try {
+	        SendMailUtil sendMailUtil = new SendMailUtil();
+	        //sendMailUtil.sendEmailTo("greenstan@naver.com", "테스트 이메일", "안녕하세요, 이것은 테스트 이메일입니다.");
+	    } catch (Exception e) {
+	        e.printStackTrace(); // 예외가 발생하면 로그에 출력
+	    }
+		
 		super.gotoPage("member/userAuthStartForm.jsp");
 	}
 
