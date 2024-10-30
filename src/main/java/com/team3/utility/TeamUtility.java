4c9f376fc8e78a368281b4bf91abcb0ab6568531
stanley <greenstan@naver.com>
2024-10-30 16:40:43 +0900

    Aibbs 관련 수정

--- src/main/java/com/team3/utility/TeamUtility.java
+++ src/main/java/com/team3/utility/TeamUtility.java
@@ -199,11 +199,11 @@ public class TeamUtility {
 		opsMap.put("userCardParse", "com.team3.controller.member.UserCardAuthController");
 
 		// aibbs section
-		//opsMap.put("aibbsInsert", "com.team3.controller.aibbs.AibbsInsertController");
-		//opsMap.put("aibbsDetail", "com.team3.controller.aibbs.AibbsDetailController");
-		//opsMap.put("aibbsUpdate", "com.team3.controller.aibbs.AibbsUpdateController");
-		//opsMap.put("aibbsDelete", "com.team3.controller.aibbs.AibbsDeleteController");
-		//opsMap.put("aibbsList",   "com.team3.controller.aibbs.AibbsListController");
+		opsMap.put("aibbsInsert", "com.team3.controller.aibbs.AibbsInsertController");
+		opsMap.put("aibbsDetail", "com.team3.controller.aibbs.AibbsDetailController");
+		opsMap.put("aibbsUpdate", "com.team3.controller.aibbs.AibbsUpdateController");
+		opsMap.put("aibbsDelete", "com.team3.controller.aibbs.AibbsDeleteController");
+		opsMap.put("aibbsList",   "com.team3.controller.aibbs.AibbsListController");
 		
 		// notice section
 		opsMap.put("noticeList", "com.team3.controller.notice.NoticeListController");
